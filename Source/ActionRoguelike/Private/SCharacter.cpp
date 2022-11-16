cebd388fcb5ff09c03e7b34cea9538445e5ed9fe
Zerymar <118243914+Zerymar@users.noreply.github.com>
2022-11-15 21:46:48 -0800

    Revert "Reverting for .gitignore"
    This reverts commit afdae87c07baa53318926b9d0764e0d98f2cdf30..

---
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+// Sets default values
+ASCharacter::ASCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Attach the SCharacter to our SpringArmComponent
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	// Attach the SpringArmComponent to our CameraComponent
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
+}
+
+// Called when the game starts or when spawned
+void ASCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+void ASCharacter::MoveForward(float Value)
+{
+	// Gets the vector pointing from the actor
+	/*
+	 *   OO
+	 *  \|/         Forward Vector 
+	 *   |          -------->
+	 *  /\
+	 *
+	 *
+	 */
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+// Called every frame
+void ASCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	// Name of our AxisComponent, Bind the movement to this character, call the function
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+
+}
+
