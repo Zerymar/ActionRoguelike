33b7005e2f67a9de4c49db4f239b3b5a06824ceb
Zerymar <118243914+Zerymar@users.noreply.github.com>
2022-11-18 17:00:09 -0800

    GameplayInterface
    added SGameplay_Interface as well as a basic itemchest implementation

---
+++ Source/ActionRoguelike/Public/SGameplayInterface.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "SGameplayInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class USGameplayInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ACTIONROGUELIKE_API ISGameplayInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	//BlueprintImplementableEvent -> Only use if you plan to implement in the Blueprint UI and not C++
+	// BlueprintNativeEvent -> Expects C++ implementation but allows for Blueprint 
+	UFUNCTION(BlueprintNativeEvent)
+	void Interact(APawn* InstigatorPawn);
+	
+};
